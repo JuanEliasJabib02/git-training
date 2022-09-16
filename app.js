fe36863102e8b3807fe069da2783f9c144dd7275
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2022-09-16 12:22:53 -0500

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("git init") // star git on the repository
+console.log("second commit ") // start git on the repository
+
+
+// git add . --> Add all files to the staging area
+ 
+// git commit -m "text" -->   send the version to the local repository
+
+
+// git am -m "text" --> is the git add and commit in the same time
+
+// git push --> send the version to the cloud repository (github)
