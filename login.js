fc75ba5bef9f11c0b5950fe8ea08af24d38fc9db
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2022-09-16 12:32:34 -0500

    doing login 3 commit

---
+++ login.js
@@ -0,0 +1,10 @@
+const login  = (email,pass) => {
+
+    if(email && pass){
+      console.log("sucesfull login")
+    } else{
+      console.log("something got wrong")
+    }
+}
+
+login("juaneliasjabib02@gmail.com","easypass1234")
